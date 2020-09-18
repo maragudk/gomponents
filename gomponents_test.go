28b2eb15be483678c86f6f2b800ec462318546c9
Markus Wüstenberg <markus@wustenberg.dk>
2020-09-18 14:32:29 +0200

    Make NodeFunc and attr implement fmt.Stringer

--- gomponents_test.go
+++ gomponents_test.go
@@ -4,17 +4,18 @@ import (
 	"testing"
 
 	g "github.com/maragudk/gomponents"
+	"github.com/maragudk/gomponents/assert"
 )
 
 func TestAttr(t *testing.T) {
 	t.Run("renders just the local name with one argument", func(t *testing.T) {
 		a := g.Attr("required")
-		equal(t, " required", a.Render())
+		assert.Equal(t, " required", a)
 	})
 
 	t.Run("renders the name and value when given two arguments", func(t *testing.T) {
 		a := g.Attr("id", "hat")
-		equal(t, ` id="hat"`, a.Render())
+		assert.Equal(t, ` id="hat"`, a)
 	})
 
 	t.Run("panics with more than two arguments", func(t *testing.T) {
@@ -34,42 +35,35 @@ func TestAttr(t *testing.T) {
 func TestEl(t *testing.T) {
 	t.Run("renders an empty element if no children given", func(t *testing.T) {
 		e := g.El("div")
-		equal(t, "<div/>", e.Render())
+		assert.Equal(t, "<div/>", e)
 	})
 
 	t.Run("renders an empty element if only attributes given as children", func(t *testing.T) {
 		e := g.El("div", g.Attr("class", "hat"))
-		equal(t, `<div class="hat"/>`, e.Render())
+		assert.Equal(t, `<div class="hat"/>`, e)
 	})
 
 	t.Run("renders an element, attributes, and element children", func(t *testing.T) {
 		e := g.El("div", g.Attr("class", "hat"), g.El("span"))
-		equal(t, `<div class="hat"><span/></div>`, e.Render())
+		assert.Equal(t, `<div class="hat"><span/></div>`, e)
 	})
 
 	t.Run("renders attributes at the correct place regardless of placement in parameter list", func(t *testing.T) {
 		e := g.El("div", g.El("span"), g.Attr("class", "hat"))
-		equal(t, `<div class="hat"><span/></div>`, e.Render())
+		assert.Equal(t, `<div class="hat"><span/></div>`, e)
 	})
 }
 
 func TestText(t *testing.T) {
 	t.Run("renders escaped text", func(t *testing.T) {
 		e := g.Text("<div/>")
-		equal(t, "&lt;div/&gt;", e.Render())
+		assert.Equal(t, "&lt;div/&gt;", e)
 	})
 }
 
 func TestRaw(t *testing.T) {
 	t.Run("renders raw text", func(t *testing.T) {
 		e := g.Raw("<div/>")
-		equal(t, "<div/>", e.Render())
+		assert.Equal(t, "<div/>", e)
 	})
-}
-
-func equal(t *testing.T, expected, actual string) {
-	if expected != actual {
-		t.Errorf("expected %v but got %v", expected, actual)
-		t.FailNow()
-	}
 }
