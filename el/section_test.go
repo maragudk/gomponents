4448a48d85e15895ce68c3d2b9ca009a222fcd2a
Markus Wüstenberg <markus@maragu.dk>
2020-09-25 17:36:21 +0200

    Add remaining sectioning elements as helpers (#25)
    Fixes #22.

--- el/section_test.go
+++ el/section_test.go
@@ -8,6 +8,36 @@ import (
 	"github.com/maragudk/gomponents/el"
 )
 
+func TestAddress(t *testing.T) {
+	t.Run("returns an address element", func(t *testing.T) {
+		assert.Equal(t, `<address />`, el.Address())
+	})
+}
+
+func TestArticle(t *testing.T) {
+	t.Run("returns an article element", func(t *testing.T) {
+		assert.Equal(t, `<article />`, el.Article())
+	})
+}
+
+func TestAside(t *testing.T) {
+	t.Run("returns an aside element", func(t *testing.T) {
+		assert.Equal(t, `<aside />`, el.Aside())
+	})
+}
+
+func TestFooter(t *testing.T) {
+	t.Run("returns a footer element", func(t *testing.T) {
+		assert.Equal(t, `<footer />`, el.Footer())
+	})
+}
+
+func TestHeader(t *testing.T) {
+	t.Run("returns a header element", func(t *testing.T) {
+		assert.Equal(t, `<header />`, el.Header())
+	})
+}
+
 func TestH1(t *testing.T) {
 	t.Run("returns an h1 element", func(t *testing.T) {
 		assert.Equal(t, `<h1 id="headline">hat</h1>`, el.H1("hat", g.Attr("id", "headline")))
@@ -41,5 +71,29 @@ func TestH5(t *testing.T) {
 func TestH6(t *testing.T) {
 	t.Run("returns an h6 element", func(t *testing.T) {
 		assert.Equal(t, `<h6 id="headline">hat</h6>`, el.H6("hat", g.Attr("id", "headline")))
+	})
+}
+
+func TestHGroup(t *testing.T) {
+	t.Run("returns an hgroup element", func(t *testing.T) {
+		assert.Equal(t, `<hgroup />`, el.HGroup())
+	})
+}
+
+func TestMainEl(t *testing.T) {
+	t.Run("returns a main element", func(t *testing.T) {
+		assert.Equal(t, `<main />`, el.Main())
+	})
+}
+
+func TestNav(t *testing.T) {
+	t.Run("returns a nav element", func(t *testing.T) {
+		assert.Equal(t, `<nav />`, el.Nav())
+	})
+}
+
+func TestSection(t *testing.T) {
+	t.Run("returns a section element", func(t *testing.T) {
+		assert.Equal(t, `<section />`, el.Section())
 	})
 }
