d8385e6db55bd93e87bea09a5bd4660fbdfd64fb
Markus Wüstenberg <markus@maragu.dk>
2020-10-28 16:45:20 +0100

    Add attribute helpers
    Also refactor tests to be table-driven, for readability.

--- attr/attributes_test.go
+++ attr/attributes_test.go
@@ -8,18 +8,6 @@ import (
 	"github.com/maragudk/gomponents/attr"
 )
 
-func TestID(t *testing.T) {
-	t.Run("given a value, returns id=value", func(t *testing.T) {
-		assert.Equal(t, ` id="hat"`, attr.ID("hat"))
-	})
-}
-
-func TestClass(t *testing.T) {
-	t.Run("given a value, returns class=value", func(t *testing.T) {
-		assert.Equal(t, ` class="hat"`, attr.Class("hat"))
-	})
-}
-
 func TestClasses(t *testing.T) {
 	t.Run("given a map, returns sorted keys from the map with value true", func(t *testing.T) {
 		assert.Equal(t, ` class="boheme-hat hat partyhat"`, attr.Classes{
---
+++ attr/boolean_test.go
@@ -0,0 +1,32 @@
+package attr_test
+
+import (
+	"fmt"
+	"testing"
+
+	g "github.com/maragudk/gomponents"
+	"github.com/maragudk/gomponents/assert"
+	"github.com/maragudk/gomponents/attr"
+)
+
+func TestBooleanAttributes(t *testing.T) {
+	cases := map[string]func() g.Node{
+		"async":     attr.Async,
+		"autofocus": attr.AutoFocus,
+		"autoplay":  attr.AutoPlay,
+		"controls":  attr.Controls,
+		"defer":     attr.Defer,
+		"disabled":  attr.Disabled,
+		"multiple":  attr.Multiple,
+		"readonly":  attr.ReadOnly,
+		"required":  attr.Required,
+		"selected":  attr.Selected,
+	}
+
+	for name, fn := range cases {
+		t.Run(fmt.Sprintf("should output %v", name), func(t *testing.T) {
+			n := g.El("div", fn())
+			assert.Equal(t, fmt.Sprintf(`<div %v />`, name), n)
+		})
+	}
+}
