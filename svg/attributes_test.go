8288ce2642bf50f62cc20888ab112b874a7c1b6d
Markus Wüstenberg <markus@maragu.dk>
2021-05-05 09:19:04 +0200

    Add svg package and a few helpers
    Introducing SVG, to inline vector graphics in your HTML! 🥳

---
+++ svg/attributes_test.go
@@ -0,0 +1,28 @@
+package svg_test
+
+import (
+	"fmt"
+	"testing"
+
+	g "github.com/maragudk/gomponents"
+	"github.com/maragudk/gomponents/assert"
+	. "github.com/maragudk/gomponents/svg"
+)
+
+func TestSimpleAttributes(t *testing.T) {
+	cases := map[string]func(string) g.Node{
+		"clip-rule": ClipRule,
+		"d":         D,
+		"fill":      Fill,
+		"fill-rule": FillRule,
+		"stroke":    Stroke,
+		"viewBox":   ViewBox,
+	}
+
+	for name, fn := range cases {
+		t.Run(fmt.Sprintf(`should output %v="hat"`, name), func(t *testing.T) {
+			n := g.El("element", fn("hat"))
+			assert.Equal(t, fmt.Sprintf(`<element %v="hat"></element>`, name), n)
+		})
+	}
+}
