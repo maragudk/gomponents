1ef94ff7a292343946f706762359d92c4a51ea78
Markus Wüstenberg <markus@maragu.dk>
2021-06-08 16:50:44 +0200

    Add LinkStylesheet and LinkPreload components
    These are often used in the `<head>` part of the HTML.

--- components/elements_test.go
+++ components/elements_test.go
@@ -14,3 +14,17 @@ func TestInputHidden(t *testing.T) {
 		assert.Equal(t, `<input type="hidden" name="id" value="partyhat" class="hat">`, n)
 	})
 }
+
+func TestLinkStylesheet(t *testing.T) {
+	t.Run("returns a link element with rel stylesheet and the given href", func(t *testing.T) {
+		n := c.LinkStylesheet("style.css", g.Attr("media", "print"))
+		assert.Equal(t, `<link rel="stylesheet" href="style.css" media="print">`, n)
+	})
+}
+
+func TestLinkPreload(t *testing.T) {
+	t.Run("returns a link element with rel preload and the given href and as", func(t *testing.T) {
+		n := c.LinkPreload("party.woff2", "font", g.Attr("type", "font/woff2"))
+		assert.Equal(t, `<link rel="preload" href="party.woff2" as="font" type="font/woff2">`, n)
+	})
+}
