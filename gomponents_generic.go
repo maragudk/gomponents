26277081ae7e3cf9eb8c0b4899aa017d8d682c35
Markus Wüstenberg <markus@maragu.dk>
2023-05-11 11:36:46 +0200

    Add MapIndex function
    I've often found myself needing the index when mapping something to nodes, so here's a `MapIndex` function.

--- gomponents_generic.go
+++ gomponents_generic.go
@@ -11,3 +11,12 @@ func Map[T any](ts []T, cb func(T) Node) []Node {
 	}
 	return nodes
 }
+
+// MapIndex is like Map, but also passes the index to the callback function.
+func MapIndex[T any](ts []T, cb func(T, int) Node) []Node {
+	var nodes []Node
+	for i, t := range ts {
+		nodes = append(nodes, cb(t, i))
+	}
+	return nodes
+}
--- gomponents_generic_test.go
+++ gomponents_generic_test.go
@@ -4,6 +4,7 @@
 package gomponents_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -14,8 +15,8 @@ import (
 func TestMap(t *testing.T) {
 	t.Run("maps slices to nodes", func(t *testing.T) {
 		items := []string{"hat", "partyhat", "turtlehat"}
-		lis := g.Map(items, func(i string) g.Node {
-			return g.El("li", g.Text(i))
+		lis := g.Map(items, func(item string) g.Node {
+			return g.El("li", g.Text(item))
 		})
 
 		list := g.El("ul", lis...)
@@ -26,9 +27,35 @@ func TestMap(t *testing.T) {
 
 func ExampleMap() {
 	items := []string{"party hat", "super hat"}
-	e := g.El("ul", g.Group(g.Map(items, func(i string) g.Node {
-		return g.El("li", g.Text(i))
+	e := g.El("ul", g.Group(g.Map(items, func(item string) g.Node {
+		return g.El("li", g.Text(item))
 	})))
 	_ = e.Render(os.Stdout)
 	// Output: <ul><li>party hat</li><li>super hat</li></ul>
+}
+
+func TestMapIndex(t *testing.T) {
+	t.Run("maps slices to nodes", func(t *testing.T) {
+		items := []string{"hat", "partyhat", "turtlehat"}
+		lis := g.MapIndex(items, func(item string, i int) g.Node {
+			return g.El("li", g.Attr("data-index", fmt.Sprint(i)), g.Text(item))
+		})
+
+		list := g.El("ul", lis...)
+
+		assert.Equal(t, `<ul><li data-index="0">hat</li><li data-index="1">partyhat</li><li data-index="2">turtlehat</li></ul>`, list)
+	})
+}
+
+func ExampleMapIndex() {
+	items := []string{"party hat", "super hat"}
+	e := g.El("ul", g.Group(g.MapIndex(items, func(item string, i int) g.Node {
+		return g.El("li",
+			g.If(i%2 == 0, g.Attr("class", "even")),
+			g.If(i%2 == 1, g.Attr("class", "odd")),
+			g.Text(item),
+		)
+	})))
+	_ = e.Render(os.Stdout)
+	// Output: <ul><li class="even">party hat</li><li class="odd">super hat</li></ul>
 }
