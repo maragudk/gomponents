dcf55aa66a50d8e91f6fa46161a4c9edd852dcb0
tevintchuinkam <tevintchuinkam@gmail.com>
2024-08-30 10:05:08 +0200

    add svg attributes

--- svg/attributes.go
+++ svg/attributes.go
@@ -20,14 +20,692 @@ func FillRule(v string) g.Node {
 	return g.Attr("fill-rule", v)
 }
 
+func ViewBox(v string) g.Node {
+	return g.Attr("viewBox", v)
+}
+
+func Height(v string) g.Node {
+	return g.Attr("height", v)
+}
+
+func Width(v string) g.Node {
+	return g.Attr("width", v)
+}
+
+func X(v string) g.Node {
+	return g.Attr("x", v)
+}
+
+func Y(v string) g.Node {
+	return g.Attr("y", v)
+}
+
+func Cx(v string) g.Node {
+	return g.Attr("cx", v)
+}
+
+func Cy(v string) g.Node {
+	return g.Attr("cy", v)
+}
+
+func R(v string) g.Node {
+	return g.Attr("r", v)
+}
+
+func Rx(v string) g.Node {
+	return g.Attr("rx", v)
+}
+
+func Ry(v string) g.Node {
+	return g.Attr("ry", v)
+}
+
+func Opacity(v string) g.Node {
+	return g.Attr("opacity", v)
+}
+
+func FillOpacity(v string) g.Node {
+	return g.Attr("fill-opacity", v)
+}
+
+func FontSize(v string) g.Node {
+	return g.Attr("font-size", v)
+}
+
+func FontFamily(v string) g.Node {
+	return g.Attr("font-family", v)
+}
+
+func PathLength(v string) g.Node {
+	return g.Attr("pathLength", v)
+}
+
+func Accumulate(v string) g.Node {
+	return g.Attr("accumulate", v)
+}
+
+func Additive(v string) g.Node {
+	return g.Attr("additive", v)
+}
+
+func AlignmentBaseline(v string) g.Node {
+	return g.Attr("alignment-baseline", v)
+}
+
+func Amplitude(v string) g.Node {
+	return g.Attr("amplitude", v)
+}
+
+func Azimuth(v string) g.Node {
+	return g.Attr("azimuth", v)
+}
+
+func BaseFrequency(v string) g.Node {
+	return g.Attr("baseFrequency", v)
+}
+
+func BaselineShift(v string) g.Node {
+	return g.Attr("baseline-shift", v)
+}
+
+func Begin(v string) g.Node {
+	return g.Attr("begin", v)
+}
+
+func Bias(v string) g.Node {
+	return g.Attr("bias", v)
+}
+
+func By(v string) g.Node {
+	return g.Attr("by", v)
+}
+
+func CalcMode(v string) g.Node {
+	return g.Attr("calcMode", v)
+}
+
+func ClipPath(v string) g.Node {
+	return g.Attr("clip-path", v)
+}
+
+func ClipPathUnits(v string) g.Node {
+	return g.Attr("clipPathUnits", v)
+}
+
+func Color(v string) g.Node {
+	return g.Attr("color", v)
+}
+
+func ColorInterpolation(v string) g.Node {
+	return g.Attr("color-interpolation", v)
+}
+
+func ColorInterpolationFilters(v string) g.Node {
+	return g.Attr("color-interpolation-filters", v)
+}
+
+func Cursor(v string) g.Node {
+	return g.Attr("cursor", v)
+}
+
+func DiffuseConstant(v string) g.Node {
+	return g.Attr("diffuseConstant", v)
+}
+
+func Direction(v string) g.Node {
+	return g.Attr("direction", v)
+}
+
+func Display(v string) g.Node {
+	return g.Attr("display", v)
+}
+
+func Divisor(v string) g.Node {
+	return g.Attr("divisor", v)
+}
+
+func DominantBaseline(v string) g.Node {
+	return g.Attr("dominant-baseline", v)
+}
+
+func Dur(v string) g.Node {
+	return g.Attr("dur", v)
+}
+
+func Dx(v string) g.Node {
+	return g.Attr("dx", v)
+}
+
+func Dy(v string) g.Node {
+	return g.Attr("dy", v)
+}
+
+func EdgeMode(v string) g.Node {
+	return g.Attr("edgeMode", v)
+}
+
+func Elevation(v string) g.Node {
+	return g.Attr("elevation", v)
+}
+
+func End(v string) g.Node {
+	return g.Attr("end", v)
+}
+
+func Exponent(v string) g.Node {
+	return g.Attr("exponent", v)
+}
+
+func Filter(v string) g.Node {
+	return g.Attr("filter", v)
+}
+
+func FilterUnits(v string) g.Node {
+	return g.Attr("filterUnits", v)
+}
+
+func FloodColor(v string) g.Node {
+	return g.Attr("flood-color", v)
+}
+
+func FloodOpacity(v string) g.Node {
+	return g.Attr("flood-opacity", v)
+}
+
+func FontSizeAdjust(v string) g.Node {
+	return g.Attr("font-size-adjust", v)
+}
+
+func FontStretch(v string) g.Node {
+	return g.Attr("font-stretch", v)
+}
+
+func FontStyle(v string) g.Node {
+	return g.Attr("font-style", v)
+}
+
+func FontVariant(v string) g.Node {
+	return g.Attr("font-variant", v)
+}
+
+func FontWeight(v string) g.Node {
+	return g.Attr("font-weight", v)
+}
+
+func Fr(v string) g.Node {
+	return g.Attr("fr", v)
+}
+
+func From(v string) g.Node {
+	return g.Attr("from", v)
+}
+
+func Fx(v string) g.Node {
+	return g.Attr("fx", v)
+}
+
+func Fy(v string) g.Node {
+	return g.Attr("fy", v)
+}
+
+func Href(v string) g.Node {
+	return g.Attr("href", v)
+}
+
+func Id(v string) g.Node {
+	return g.Attr("id", v)
+}
+
+func ImageRendering(v string) g.Node {
+	return g.Attr("image-rendering", v)
+}
+
+func In(v string) g.Node {
+	return g.Attr("in", v)
+}
+
+func In2(v string) g.Node {
+	return g.Attr("in2", v)
+}
+
+func Intercept(v string) g.Node {
+	return g.Attr("intercept", v)
+}
+
+func K1(v string) g.Node {
+	return g.Attr("k1", v)
+}
+
+func K2(v string) g.Node {
+	return g.Attr("k2", v)
+}
+
+func K3(v string) g.Node {
+	return g.Attr("k3", v)
+}
+
+func K4(v string) g.Node {
+	return g.Attr("k4", v)
+}
+
+func KernelMatrix(v string) g.Node {
+	return g.Attr("kernelMatrix", v)
+}
+
+func KernelUnitLength(v string) g.Node {
+	return g.Attr("kernelUnitLength", v)
+}
+
+func KeyPoints(v string) g.Node {
+	return g.Attr("keyPoints", v)
+}
+
+func KeySplines(v string) g.Node {
+	return g.Attr("keySplines", v)
+}
+
+func KeyTimes(v string) g.Node {
+	return g.Attr("keyTimes", v)
+}
+
+func Lang(v string) g.Node {
+	return g.Attr("lang", v)
+}
+
+func LengthAdjust(v string) g.Node {
+	return g.Attr("lengthAdjust", v)
+}
+
+func LetterSpacing(v string) g.Node {
+	return g.Attr("letter-spacing", v)
+}
+
+func LightingColor(v string) g.Node {
+	return g.Attr("lighting-color", v)
+}
+
+func LimitingConeAngle(v string) g.Node {
+	return g.Attr("limitingConeAngle", v)
+}
+
+func MarkerEnd(v string) g.Node {
+	return g.Attr("marker-end", v)
+}
+
+func MarkerMid(v string) g.Node {
+	return g.Attr("marker-mid", v)
+}
+
+func MarkerStart(v string) g.Node {
+	return g.Attr("marker-start", v)
+}
+
+func MarkerHeight(v string) g.Node {
+	return g.Attr("markerHeight", v)
+}
+
+func MarkerUnits(v string) g.Node {
+	return g.Attr("markerUnits", v)
+}
+
+func MarkerWidth(v string) g.Node {
+	return g.Attr("markerWidth", v)
+}
+
+func Mask(v string) g.Node {
+	return g.Attr("mask", v)
+}
+
+func MaskContentUnits(v string) g.Node {
+	return g.Attr("maskContentUnits", v)
+}
+
+func MaskUnits(v string) g.Node {
+	return g.Attr("maskUnits", v)
+}
+
+// ... existing code ...
+
+func Max(v string) g.Node {
+	return g.Attr("max", v)
+}
+
+func Media(v string) g.Node {
+	return g.Attr("media", v)
+}
+
+func Min(v string) g.Node {
+	return g.Attr("min", v)
+}
+
+func Mode(v string) g.Node {
+	return g.Attr("mode", v)
+}
+
+func NumOctaves(v string) g.Node {
+	return g.Attr("numOctaves", v)
+}
+
+func Operator(v string) g.Node {
+	return g.Attr("operator", v)
+}
+
+func Order(v string) g.Node {
+	return g.Attr("order", v)
+}
+
+func Orient(v string) g.Node {
+	return g.Attr("orient", v)
+}
+
+func Origin(v string) g.Node {
+	return g.Attr("origin", v)
+}
+
+func Overflow(v string) g.Node {
+	return g.Attr("overflow", v)
+}
+
+func OverlinePosition(v string) g.Node {
+	return g.Attr("overline-position", v)
+}
+
+func OverlineThickness(v string) g.Node {
+	return g.Attr("overline-thickness", v)
+}
+
+func PaintOrder(v string) g.Node {
+	return g.Attr("paint-order", v)
+}
+
+func PatternContentUnits(v string) g.Node {
+	return g.Attr("patternContentUnits", v)
+}
+
+func PatternTransform(v string) g.Node {
+	return g.Attr("patternTransform", v)
+}
+
+func PatternUnits(v string) g.Node {
+	return g.Attr("patternUnits", v)
+}
+
+func PointerEvents(v string) g.Node {
+	return g.Attr("pointer-events", v)
+}
+
+func Points(v string) g.Node {
+	return g.Attr("points", v)
+}
+
+func PointsAtX(v string) g.Node {
+	return g.Attr("pointsAtX", v)
+}
+
+func PointsAtY(v string) g.Node {
+	return g.Attr("pointsAtY", v)
+}
+
+func PointsAtZ(v string) g.Node {
+	return g.Attr("pointsAtZ", v)
+}
+
+func PreserveAlpha(v string) g.Node {
+	return g.Attr("preserveAlpha", v)
+}
+
+func PreserveAspectRatio(v string) g.Node {
+	return g.Attr("preserveAspectRatio", v)
+}
+
+func PrimitiveUnits(v string) g.Node {
+	return g.Attr("primitiveUnits", v)
+}
+
+func Radius(v string) g.Node {
+	return g.Attr("radius", v)
+}
+
+func RefX(v string) g.Node {
+	return g.Attr("refX", v)
+}
+
+func RefY(v string) g.Node {
+	return g.Attr("refY", v)
+}
+
+func RepeatCount(v string) g.Node {
+	return g.Attr("repeatCount", v)
+}
+
+func RepeatDur(v string) g.Node {
+	return g.Attr("repeatDur", v)
+}
+
+func Restart(v string) g.Node {
+	return g.Attr("restart", v)
+}
+
+func Result(v string) g.Node {
+	return g.Attr("result", v)
+}
+
+func Scale(v string) g.Node {
+	return g.Attr("scale", v)
+}
+
+func Seed(v string) g.Node {
+	return g.Attr("seed", v)
+}
+
+func ShapeRendering(v string) g.Node {
+	return g.Attr("shape-rendering", v)
+}
+
+func Slope(v string) g.Node {
+	return g.Attr("slope", v)
+}
+
+func Spacing(v string) g.Node {
+	return g.Attr("spacing", v)
+}
+
+func SpecularConstant(v string) g.Node {
+	return g.Attr("specularConstant", v)
+}
+
+func SpecularExponent(v string) g.Node {
+	return g.Attr("specularExponent", v)
+}
+
+func SpreadMethod(v string) g.Node {
+	return g.Attr("spreadMethod", v)
+}
+
+func StartOffset(v string) g.Node {
+	return g.Attr("startOffset", v)
+}
+
+func StdDeviation(v string) g.Node {
+	return g.Attr("stdDeviation", v)
+}
+
+func StitchTiles(v string) g.Node {
+	return g.Attr("stitchTiles", v)
+}
+
+func StopColor(v string) g.Node {
+	return g.Attr("stop-color", v)
+}
+
+func StopOpacity(v string) g.Node {
+	return g.Attr("stop-opacity", v)
+}
+
+func StrikethroughPosition(v string) g.Node {
+	return g.Attr("strikethrough-position", v)
+}
+
+func StrikethroughThickness(v string) g.Node {
+	return g.Attr("strikethrough-thickness", v)
+}
+
 func Stroke(v string) g.Node {
 	return g.Attr("stroke", v)
+}
+
+func StrokeDasharray(v string) g.Node {
+	return g.Attr("stroke-dasharray", v)
+}
+
+func StrokeDashoffset(v string) g.Node {
+	return g.Attr("stroke-dashoffset", v)
+}
+
+func StrokeLinecap(v string) g.Node {
+	return g.Attr("stroke-linecap", v)
+}
+
+func StrokeLinejoin(v string) g.Node {
+	return g.Attr("stroke-linejoin", v)
+}
+
+func StrokeMiterlimit(v string) g.Node {
+	return g.Attr("stroke-miterlimit", v)
+}
+
+func StrokeOpacity(v string) g.Node {
+	return g.Attr("stroke-opacity", v)
 }
 
 func StrokeWidth(v string) g.Node {
 	return g.Attr("stroke-width", v)
 }
 
-func ViewBox(v string) g.Node {
-	return g.Attr("viewBox", v)
+func Style(v string) g.Node {
+	return g.Attr("style", v)
+}
+
+func SurfaceScale(v string) g.Node {
+	return g.Attr("surfaceScale", v)
+}
+
+func Crossorigin(v string) g.Node {
+	return g.Attr("crossorigin", v)
+}
+
+func SystemLanguage(v string) g.Node {
+	return g.Attr("systemLanguage", v)
+}
+
+func Tabindex(v string) g.Node {
+	return g.Attr("tabindex", v)
+}
+
+func TableValues(v string) g.Node {
+	return g.Attr("tableValues", v)
+}
+
+func Target(v string) g.Node {
+	return g.Attr("target", v)
+}
+
+func TargetX(v string) g.Node {
+	return g.Attr("targetX", v)
+}
+
+func TargetY(v string) g.Node {
+	return g.Attr("targetY", v)
+}
+
+func TextAnchor(v string) g.Node {
+	return g.Attr("text-anchor", v)
+}
+
+func TextDecoration(v string) g.Node {
+	return g.Attr("text-decoration", v)
+}
+
+func TextRendering(v string) g.Node {
+	return g.Attr("text-rendering", v)
+}
+
+func TextLength(v string) g.Node {
+	return g.Attr("textLength", v)
+}
+
+func To(v string) g.Node {
+	return g.Attr("to", v)
+}
+
+func Transform(v string) g.Node {
+	return g.Attr("transform", v)
+}
+
+func TransformOrigin(v string) g.Node {
+	return g.Attr("transform-origin", v)
+}
+
+func Type(v string) g.Node {
+	return g.Attr("type", v)
+}
+
+func UnderlinePosition(v string) g.Node {
+	return g.Attr("underline-position", v)
+}
+
+func UnderlineThickness(v string) g.Node {
+	return g.Attr("underline-thickness", v)
+}
+
+func UnicodeBidi(v string) g.Node {
+	return g.Attr("unicode-bidi", v)
+}
+
+func Values(v string) g.Node {
+	return g.Attr("values", v)
+}
+
+func VectorEffect(v string) g.Node {
+	return g.Attr("vector-effect", v)
+}
+
+func Visibility(v string) g.Node {
+	return g.Attr("visibility", v)
+}
+
+func WordSpacing(v string) g.Node {
+	return g.Attr("word-spacing", v)
+}
+
+func WritingMode(v string) g.Node {
+	return g.Attr("writing-mode", v)
+}
+
+func X1(v string) g.Node {
+	return g.Attr("x1", v)
+}
+
+func X2(v string) g.Node {
+	return g.Attr("x2", v)
+}
+
+func XChannelSelector(v string) g.Node {
+	return g.Attr("xChannelSelector", v)
+}
+
+func Y1(v string) g.Node {
+	return g.Attr("y1", v)
+}
+
+func Y2(v string) g.Node {
+	return g.Attr("y2", v)
+}
+
+func YChannelSelector(v string) g.Node {
+	return g.Attr("yChannelSelector", v)
+}
+
+func Z(v string) g.Node {
+	return g.Attr("z", v)
 }
